7385ae799ec61c5b99e7ae6bf2fda39c629637d9
Sam <samangarc22@gmail.com>
2022-06-12 19:47:04 -0400

    Working on Moralis Integration for Login

---
+++ Assets/_Scripts/WalletLogin.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoralisUnity;
+using MoralisUnity.Platform.Objects;
+using MoralisUnity.Web3Api.Models;
+
+public class WalletLogin : MonoBehaviour
+{
+    public async void ConnectWallet() {
+        Debug.Log("Wallet Connected");
+        if (MoralisState.Initialized.Equals(Moralis.State))
+            {
+                MoralisUser user = await Moralis.GetUserAsync();
+
+                if (user == null)
+                {
+                    // User is null so go back to the authentication scene.
+                    // SceneManager.LoadScene(0);
+                }
+
+                // Display User's wallet address.
+                // addressText.text = FormatUserAddressForDisplay(user.ethAddress);
+
+                // Retrienve the user's native balance;
+                NativeBalance balanceResponse = await Moralis.Web3Api.Account.GetNativeBalance(user.ethAddress, Moralis.CurrentChain.EnumValue);
+
+                double balance = 0.0;
+                float decimals = Moralis.CurrentChain.Decimals * 1.0f;
+                string sym = Moralis.CurrentChain.Symbol;
+
+                // Make sure a response to the balanace request weas received. The 
+                // IsNullOrWhitespace check may not be necessary ...
+                if (balanceResponse != null && !string.IsNullOrWhiteSpace(balanceResponse.Balance))
+                {
+                    double.TryParse(balanceResponse.Balance, out balance);
+                }
+
+                // Display native token amount token in fractions of token.
+                // NOTE: May be better to link this to chain since some tokens may have
+                // more than 18 sigjnificant figures.
+                // balanceText.text = string.Format("{0:0.####} {1}", (balance / (double)Mathf.Pow(10.0f, decimals)), sym);
+            }
+    }
+}
